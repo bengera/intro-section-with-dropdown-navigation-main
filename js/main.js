9f0ae83336640f9d82ddee9e59db7ce2371ad142
unknown <bengera89@gmail.com>
2022-05-30 16:50:40 +0700

    menu-finish

--- js/main.js
+++ js/main.js
@@ -5,24 +5,49 @@ const overlay = document.getElementById('overlay');
 const features = document.querySelector('.list__link-features');
 const dropDown = document.querySelector('.drop-down__features');
 
+const company = document.querySelector('.list__link-company');
+const dropDown2 = document.querySelector('.drop-down__company');
+
+
+
 
 menu.addEventListener('click',() => {
     console.log('toggle-menu');
     menu.classList.toggle('active');
     navUL.classList.toggle('show');
     overlay.setAttribute("style", "display:block");
-    
+
+           
     if (menu.classList.contains('active')) {
         console.log('menu-open');
 
     } else {
         overlay.setAttribute("style", "display:none"); 
+        resetMenu(); // reset drop-downs
     }
    
 })
-
+// FEATURES DROPDOWN
 features.addEventListener('click',() => {
     console.log('toggle-features');
     dropDown.classList.toggle('show-f');
+    features.classList.toggle('show');
 })
+
+// COMPANY DROPDOWN
+company.addEventListener('click',() => {
+    console.log('toggle-company');
+    dropDown2.classList.toggle('show-f');
+    company.classList.toggle('show');
+})
+
+function resetMenu() {
+    dropDown.classList.remove('show-f'); 
+    dropDown2.classList.remove('show-f');
+    features.classList.remove('show');
+    company.classList.remove('show');
+    
+  }
+
+
 
